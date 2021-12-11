b5ba5a4088d395db92c8d579794161dc9c806a4e
WangZY <wangzeyang1999@163.com>
2021-12-11 11:56:19 +0800

    first commit

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout<<"hello world"<<endl;
+    int a = 3;
+    int b = 5;
+    if (a+b == 8)
+    {
+        cout<<((a+b) == 8);
+    }
+       
+    return 0;
+}
